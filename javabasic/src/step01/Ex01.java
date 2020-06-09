309e6701047631c1835d3678c1151071757f180b
dadomin <brlight_dm@naver.com>
2020-06-09 19:11:20 +0900

    20200609 화요일 수업

--- javabasic/src/step01/Ex01.java
+++ javabasic/src/step01/Ex01.java
@@ -1,37 +1,37 @@
 package step01;
 
 /*
- * # 과락[문제]
- * 1. 아래 세 과목의 성적 평균이 60점 이상이면 합격생이다.
- * 2. 단, 어느 한 과목이라도 점수가 50점 미만이면 불합격생이다.
- * 3. 합격 여부를 출력한다.
+ * # 怨쇰씫[臾몄젣]
+ * 1. �븘�옒 �꽭 怨쇰ぉ�쓽 �꽦�쟻 �룊洹좎씠 60�젏 �씠�긽�씠硫� �빀寃⑹깮�씠�떎.
+ * 2. �떒, �뼱�뒓 �븳 怨쇰ぉ�씠�씪�룄 �젏�닔媛� 50�젏 誘몃쭔�씠硫� 遺덊빀寃⑹깮�씠�떎.
+ * 3. �빀寃� �뿬遺�瑜� 異쒕젰�븳�떎.
  */
 
 public class Ex01 {
 	public static void main(String[] args) {
 
-		int kor = 87; // 국어 점수
-		int eng = 100; // 영어 점수
-		int math = 42; // 수학 점수
+		int kor = 87; // 援��뼱 �젏�닔
+		int eng = 100; // �쁺�뼱 �젏�닔
+		int math = 42; // �닔�븰 �젏�닔
 		
-		boolean pass = true; // 합격 여부
-		double avg = (double)(kor + eng + math) / 3; // 평균 점수
+		boolean pass = true; // �빀寃� �뿬遺�
+		double avg = (double)(kor + eng + math) / 3; // �룊洹� �젏�닔
 		
-		//평균이 60점 미만이면 불합격
+		//�룊洹좎씠 60�젏 誘몃쭔�씠硫� 遺덊빀寃�
 		if(avg < 60) {
 			pass = false;
 		}
 		
-		// 과목당 점수가 50점 미만이 하나라도 있으면 불합격
+		// 怨쇰ぉ�떦 �젏�닔媛� 50�젏 誘몃쭔�씠 �븯�굹�씪�룄 �엳�쑝硫� 遺덊빀寃�
 		if(kor < 50 || eng < 50 || math < 50) {
 			pass = false;
 		}
 		
-		//합격 여부 출력
+		//�빀寃� �뿬遺� 異쒕젰
 		if(pass) {
-			System.out.println("합격");
+			System.out.println("�빀寃�");
 		}else {
-			System.out.println("불합격");
+			System.out.println("遺덊빀寃�");
 		}
 		
 	}
